473cb01be14a0c9aa72a9c10d0ab7308cc45eb48
Peter Moise <moise.p@northeastern.edu>
2024-06-21 04:47:39 +0000

    added panda and database stuff

--- trailRun.py
+++ trailRun.py
@@ -1,12 +1,37 @@
 import requests
+import pandas as pd
+import sqlalchemy as db
 
 url = 'https://api.sportmonks.com/v3/football/fixtures'
 header = {'Authorization': '8VXrDStCmKp2kNnZHwoaYv3mMYCA3oAwJcRqxzg5f5k4k8Ek3bNv7J5AhWqd'}
 
 response = requests.get(url, headers=header)
+engine = db.create_engine('sqlite:///soccer_matches.db')
 
-print(response.status_code)
+
 huge_dict_matches = response.json()['data']
-for index in range(len(huge_dict_matches)):
-  dictionary = huge_dict_matches[index]
-  print(f"The fixture was {dictionary.get('name')} and {dictionary.get('result_info')} This happened {dictionary.get('starting_at')}")
+
+
+dictionary = {
+  'name': [],
+  'result_info': [],
+  'starting_at': []
+  }
+
+for index in range(1, len(huge_dict_matches)):
+  row = huge_dict_matches[index]
+
+  dictionary['name'].append(row.get('name'))
+  dictionary['result_info'].append(row.get('result_info'))
+  dictionary['starting_at'].append(row.get('starting_at'))
+
+pdFrame = pd.DataFrame.from_dict(dictionary)
+
+pdFrame.to_sql('soccer_matches', con=engine, if_exists='replace', index=False)
+
+with engine.connect() as connection:
+   query_result = connection.execute(db.text("SELECT * FROM soccer_matches LIMIT 3;")).fetchall()
+   print(pd.DataFrame(query_result))
+
+
+
